a78ef669f34bf278ab30d7524a6e47ca73235df5
Steve Dignam <steve@dignam.xyz>
2021-07-16 09:12:50 -0400

    cut dead import

--- typings/django/contrib/auth/models.pyi
+++ typings/django/contrib/auth/models.pyi
@@ -8,7 +8,7 @@ from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.db.models.base import Model
-from django.db.models.manager import EmptyManager, Manager
+from django.db.models.manager import EmptyManager
 
 if sys.version_info < (3, 8):
     from typing_extensions import Literal

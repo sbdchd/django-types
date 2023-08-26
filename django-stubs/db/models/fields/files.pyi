ec4537020c9785b733e15bd020aa45b2e64dbf34
Ali Hamdan <ali.hamdan.dev@gmail.com>
2023-08-26 16:34:10 +0200

    Use Self type from PEP 673 everywhere

--- django-stubs/db/models/fields/files.pyi
+++ django-stubs/db/models/fields/files.pyi
@@ -1,14 +1,5 @@
-from typing import (
-    Any,
-    Callable,
-    Iterable,
-    Optional,
-    Tuple,
-    Type,
-    TypeVar,
-    Union,
-    overload,
-)
+from typing import Any, Callable, Iterable, Optional, Tuple, Type, Union, overload
+from typing_extensions import Self
 
 from django.core.files.base import File
 from django.core.files.images import ImageFile
@@ -47,8 +38,6 @@ class FileDescriptor:
     def __get__(
         self, instance: Optional[Model], cls: Type[Model] = ...
     ) -> Union[FieldFile, FileDescriptor]: ...
-
-_T = TypeVar("_T", bound="Field[Any, Any]")
 
 class FileField(Field[FileDescriptor, FileDescriptor]):
     storage: Any = ...
@@ -89,7 +78,7 @@ class FileField(Field[FileDescriptor, FileDescriptor]):
     def __get__(self, instance: Model, owner: Any) -> FieldFile: ...
     # non-Model instances
     @overload
-    def __get__(self: _T, instance: Any, owner: Any) -> _T: ...
+    def __get__(self, instance: Any, owner: Any) -> Self: ...
     def generate_filename(self, instance: Optional[Model], filename: str) -> str: ...
 
 class ImageFileDescriptor(FileDescriptor):
@@ -139,7 +128,7 @@ class ImageField(FileField):
     def __get__(self, instance: Model, owner: Any) -> ImageFieldFile: ...
     # non-Model instances
     @overload
-    def __get__(self: _T, instance: Any, owner: Any) -> _T: ...
+    def __get__(self, instance: Any, owner: Any) -> Self: ...
     def update_dimension_fields(
         self, instance: Model, force: bool = ..., *args: Any, **kwargs: Any
     ) -> None: ...

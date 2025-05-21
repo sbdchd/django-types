85086cfbef96eecb305894c8126d5516ebc327ba
Will Horne <will.horne@cloudrunner.com>
2025-05-21 16:12:51 -0700

    Use imports compatible with older versions of python

--- django-stubs/urls/base.pyi
+++ django-stubs/urls/base.pyi
@@ -1,5 +1,6 @@
-from collections.abc import Callable, Sequence
-from typing import Any, Mapping, TypeAlias
+from collections.abc import Callable, Mapping, Sequence
+from typing import Any
+from typing_extensions import TypeAlias
 
 from django.http.request import QueryDict
 from django.urls.resolvers import ResolverMatch

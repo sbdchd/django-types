c75272d992ecee6983c534962c24ef50c2439526
Konstantin Alekseev <mail@kalekseev.com>
2026-05-05 16:38:31 +0300

    sync sitemaps from django-stubs (#378)

--- django-stubs/contrib/sitemaps/views.pyi
+++ django-stubs/contrib/sitemaps/views.pyi
@@ -1,22 +1,30 @@
-from collections import OrderedDict
-from collections.abc import Callable
-from typing import Any
+from collections.abc import Callable, Mapping
+from dataclasses import dataclass
+from typing import Any, TypeVar
 
-from django.contrib.sitemaps import GenericSitemap, Sitemap
+from django.contrib.sitemaps import Sitemap
 from django.http.request import HttpRequest
 from django.template.response import TemplateResponse
 
-def x_robots_tag(func: Callable[..., Any]) -> Callable[..., Any]: ...
+_C = TypeVar("_C", bound=Callable[..., Any])
+_T = TypeVar("_T")
+
+@dataclass
+class SitemapIndexItem:
+    location: str
+    last_mod: bool | None = ...
+
+def x_robots_tag(func: _C) -> _C: ...
 def index(
     request: HttpRequest,
-    sitemaps: dict[str, type[Sitemap] | Sitemap],
+    sitemaps: Mapping[str, type[Sitemap[_T]] | Sitemap[_T]],
     template_name: str = ...,
     content_type: str = ...,
     sitemap_url_name: str = ...,
 ) -> TemplateResponse: ...
 def sitemap(
     request: HttpRequest,
-    sitemaps: (dict[str, type[Sitemap]] | dict[str, GenericSitemap] | OrderedDict[Any, Any]),
+    sitemaps: Mapping[str, type[Sitemap[_T]] | Sitemap[_T]],
     section: str | None = ...,
     template_name: str = ...,
     content_type: str = ...,
